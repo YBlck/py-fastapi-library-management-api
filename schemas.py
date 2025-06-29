28ef354ee3119e0cde291a6d013d25b311d94e53
Yura Chornyi <chorny.yura@gmail.com>
2025-06-30 01:52:39 +0300

    feat: add schemas.py and models for Author and Book

---
+++ schemas.py
@@ -0,0 +1,42 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class AuthorBase(BaseModel):
+    name: str
+    bio: str
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class Author(AuthorBase):
+    id: int
+    books: list["Book"]
+
+    class Config:
+        from_attributes = True
+
+
+class BookBase(BaseModel):
+    title: str
+    summary: str
+    publication_date: date
+
+
+class BookCreate(BookBase):
+    author_id: int
+
+
+class Book(BookBase):
+    id: int
+    author: Author
+
+    class Config:
+        from_attributes = True
+
+
+Book.model_rebuild()
+Author.model_rebuild()
