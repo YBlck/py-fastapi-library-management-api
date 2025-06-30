b056d7bdb518506566b264527fefb3394fa2fe53
Yura Chornyi <chorny.yura@gmail.com>
2025-06-30 13:05:28 +0300

    feat: add get_db function

--- db/database.py
+++ db/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///library.db"
 
@@ -9,3 +9,12 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
