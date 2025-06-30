ac73651f2dc019350e451c1e2e0d1b9478b376ed
Yura Chornyi <chorny.yura@gmail.com>
2025-06-30 13:44:51 +0300

    feat: add Author endpoints
    - add list endpoint
    - add detail endpoint
    - add create endpoint

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Query, HTTPException, status
+from sqlalchemy.orm import Session
+
+import crud
+import schemas
+from db.database import get_db
 
 app = FastAPI()
 
@@ -6,3 +11,44 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World!"}
+
+
+@app.get("/authors/", response_model=schemas.AuthorList)
+def author_list(
+    page: int = Query(1, ge=1),
+    per_page: int = Query(5, le=20),
+    db: Session = Depends(get_db),
+):
+    offset = (page - 1) * per_page
+
+    db_authors = crud.get_author_list(db=db, skip=offset, limit=per_page)
+    total_authors = crud.get_author_count(db=db)
+    total_pages = (total_authors + per_page - 1) // per_page
+
+    return {
+        "page": page,
+        "per_page": per_page,
+        "total_pages": total_pages,
+        "authors": db_authors,
+    }
+
+
+@app.get("/authors/{author_id}", response_model=schemas.Author)
+def author_detail(author_id: int, db: Session = Depends(get_db)):
+    db_author = crud.get_author_by_id(db=db, author_id=author_id)
+
+    if db_author is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Author not found"
+        )
+
+    return db_author
+
+
+@app.post(
+    "/authors/",
+    response_model=schemas.Author,
+    status_code=status.HTTP_201_CREATED,
+)
+def author_create(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+    return crud.create_author(db=db, author=author)
